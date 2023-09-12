8e0db252590e24fc89ba20aa42cfae8728059787
Jongmin Han <jonhan@c1r6s6.42seoul.kr>
2023-09-12 16:08:08 +0900

    [fix] : move to vector operator func

--- miniRT.h
+++ miniRT.h
@@ -6,7 +6,7 @@
 /*   By: jonhan <jonhan@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/12 13:32:19 by jonhan            #+#    #+#             */
-/*   Updated: 2023/09/12 14:18:25 by jonhan           ###   ########.fr       */
+/*   Updated: 2023/09/12 16:05:26 by jonhan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,14 @@ typedef struct e_vec3
 	double	e[3];
 }	t_vec3;
 
+double	vec3_length_squared(t_vec3 v);
+double	vec3_length(t_vec3 v);
+t_vec3	vec3_init(void);
+t_vec3	vec3_create(double e0, double e1, double e2);
+t_vec3	vec3_negate(t_vec3 vec);
+t_vec3	vec3_add(t_vec3 v1, t_vec3 v2);
+t_vec3	vec3_multiply(t_vec3 v1, double t);
+t_vec3	vec3_divide(t_vec3 v, double t);
 
 
 #endif
